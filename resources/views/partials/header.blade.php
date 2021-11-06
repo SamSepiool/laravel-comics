380fec61bbff3f6b74a1a29ce7e7784cfac3370a
SamSepiool <84787501+SamSepiool@users.noreply.github.com>
2021-11-06 20:24:41 +0100

    add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <div class="top">
+        <ul class="container ">
+            <li><a href="#">DC POWER &#8480;VISA&#174;</a></li>
+            <li><a href="#">ADDITIONAL DC SITES</a></li>
+        </ul>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <div class="logo">
+                <a href="#"><img src="{{asset('images/dc-logo.png')}}" alt=""></a>
+            </div>
+            <nav>
+                <ul>
+                
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+
+                </ul>
+                
+            </nav>
+            <div class="search">
+                <label for="find">
+                    <input id="find" type="text" placeholder="Search">
+                    <button>
+                        <i class="fas fa-search"></i>
+                    </button>
+                </label>
+                
+            </div>
+        </div>
+    </div>
+</header> 
